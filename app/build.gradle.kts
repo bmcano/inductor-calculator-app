9762161e2ffebe5bacb0f5b3d54866a70758fea9
Brandon Cano <brandoncano01@gmail.com>
2025-07-30 16:52:22 -0500

    Dependency updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,19 +1,23 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.jetbrains.kotlin)
-    alias(libs.plugins.jetbrains.kotlin.compose)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.jetbrains.kotlin.plugin.compose)
+    alias(libs.plugins.google.services)
+    alias(libs.plugins.firebase.crashlytics)
 }
 
 android {
     namespace = "com.brandoncano.inductancecalculator"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.brandoncano.inductancecalculator"
-        minSdk = 21
-        targetSdk = 35
-        versionCode = 8 // for 1.2.2
-        versionName = "1.2.2"
+        minSdk = 23 // Android 6.0
+        targetSdk = 36
+        versionCode = 9 // for 1.3.0
+        versionName = "1.3.0-Develop"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
@@ -23,11 +27,14 @@ android {
     applicationVariants.configureEach {
         val suffix = if (buildType.name == "debug") ", DEBUG" else ""
         resValue("string", "version", "$versionName$suffix")
-        resValue("string", "last_updated", "3/16/2025")
+        resValue("string", "last_updated", "7/30/2025")
     }
     buildTypes {
         release {
-            isMinifyEnabled = false
+            // adb uninstall com.brandoncano.inductancecalculator
+            // adb install -r .\app\release\app-release.apk
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
@@ -38,24 +45,22 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = "17"
+    kotlin {
+        compilerOptions {
+            jvmTarget = JvmTarget.JVM_17
+        }
     }
     buildFeatures {
         compose = true
+        buildConfig = true
     }
-
     testOptions {
         unitTests.isReturnDefaultValues = true
     }
 }
 
-composeCompiler {
-    enableStrongSkippingMode = true
-}
-
 dependencies {
-    // androidx
+    // AndroidX
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.browser)
     implementation(libs.androidx.core.ktx)
@@ -69,16 +74,20 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
-    // androidx.compose
     implementation(libs.androidx.compose.runtime.livedata)
-    implementation(libs.androidx.compose.material.icons)
+    implementation(libs.androidx.compose.material.icons.extended)
     implementation(libs.androidx.compose.material3)
-    // com.google
+    // firebase
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.analytics)
+    implementation(libs.firebase.config)
+    implementation(libs.firebase.crashlytics.ndk)
+    // google
+    implementation(libs.billing.client)
     implementation(libs.gson)
     // unit testing
     testImplementation(libs.mockk)
     testImplementation(libs.junit)
     // external libraries
-    implementation(libs.ostermiller.util)
-    implementation(libs.bmcano.util)
+    implementation(libs.ostermiller.utils)
 }
